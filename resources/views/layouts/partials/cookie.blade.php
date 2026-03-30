b1d0946351349523cef77b79cd0686b34eeb96a0
Parabe11um <r.a.bazhenoff@gmail.com>
2026-03-31 00:44:01 +0300

    cookie.blade.php updated

--- resources/views/layouts/partials/cookie.blade.php
+++ resources/views/layouts/partials/cookie.blade.php
@@ -1,9 +1,14 @@
 <div id="cookie-banner" class="cookie-banner">
     <div class="cookie-container">
-        <button id="cookie-agree-button" class="btn btn-lg btn-sky !text-white !bg-[#5eb9f0] border-[#5eb9f0] hover:text-white hover:!bg-[#5eb9f0] hover:!border-[#5eb9f0] focus:shadow-[rgba(88,167,216,1)] active:text-white active:!bg-[#5eb9f0] active:border-[#5eb9f0] disabled:text-white disabled:!bg-[#5eb9f0] disabled:border-[#5eb9f0]  !rounded-[50rem] !mr-2">СОГЛАСЕН</button>
+        <button id="cookie-agree-button"
+                class="btn btn-lg btn-sky !text-white !bg-[#5eb9f0] border-[#5eb9f0] hover:text-white hover:!bg-[#5eb9f0] hover:!border-[#5eb9f0] focus:shadow-[rgba(88,167,216,1)] active:text-white active:!bg-[#5eb9f0] active:border-[#5eb9f0] disabled:text-white disabled:!bg-[#5eb9f0] disabled:border-[#5eb9f0]  !rounded-[50rem] !mr-2">
+            ПРИНЯТЬ
+        </button>
         <p class="cookie-text">
-            Настоящий веб-сайт использует файлы «cookie». Продолжая использовать сайт, вы соглашаетесь с
-            <a href="/terms" target="_blank">Политикой обработки персональных данных</a>.
+            Мы используем файлы cookie, чтобы сделать работу с сайтом максимально удобной. Нажимая "Принять" или,
+            продолжая пользоваться сайтом, вы даете согласие на обработку своих персональных данных (файлов cookie) для
+            работы сайта, анализа посещаемости и настройки рекламы, и соглашаетесь с нашей
+            <a href="/terms" target="_blank">политикой конфиденциальности</a>. При этом вы можете настроить свои предпочтения в настройках браузера.
         </p>
     </div>
 </div>
