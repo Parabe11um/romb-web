a9af12fa40f5725d2d356580805247b710801c40
Parabe11um <r.a.bazhenoff@gmail.com>
2026-03-31 22:30:52 +0300

    services common page - progress steps centered

--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -146,7 +146,7 @@
                     </p>
                 </div>
 
-                <div class="flex flex-wrap mx-[-15px] xl:mx-[-35px] lg:mx-[-20px] !mt-[-30px] process-wrapper line">
+                <div class="flex flex-wrap justify-center mx-0 lg:mx-[-20px] xl:mx-[-35px] !mt-[-30px] process-wrapper line">
 
                     <!-- 01 -->
                     <div class="md:w-6/12 lg:w-3/12 xl:w-3/12 w-full flex-[0_0_auto] !px-[15px] xl:!px-[35px] lg:!px-[20px] !mt-[30px] max-w-full !relative after:w-full after:absolute after:content-[''] after:h-px after:z-[1] after:border-t-[rgba(164,174,198,0.2)] after:border-t after:border-solid after:left-[3rem] after:top-6 after:bg-inherit max-lg:after:!hidden">
